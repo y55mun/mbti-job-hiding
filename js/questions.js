a79008fdc092cd9ad9d01b7e8459bfa4612bba73
User <y55mun33@gmail.com>
2023-01-02 22:11:42 +0900

    질문 js 추가

--- js/questions.js
+++ js/questions.js
@@ -18,4 +18,25 @@ function renderQuestion() {
   progressValueEl.style.width = (currentNumber + 1) * 10 + '%'
 }
 
+function nextQuestion(choiceNumber) {
+  if(currentNumber == questions.length - 1) {
+    showResultPage()
+    return
+  }
+  const question = questions[currentNumber]
+  mbti = mbti + question.choices[choiceNumber].value
+  currentNumber = currentNumber + 1
+  renderQuestion()
+}
+function showResultPage() {
+  location.href = '/results.html?mbti=' + mbti
+}
+
+choice1El.addEventListener('click', function() {
+  nextQuestion(0);
+})
+choice2El.addEventListener('click', function() {
+  nextQuestion(1);
+})
+
 renderQuestion()
